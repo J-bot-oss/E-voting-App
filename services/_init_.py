ab7605f1d754bb3cdc7bf6cec583ecfbd0128335
Jesse <jesseebong656@gmail.com>
2026-03-13 19:53:10 +0300

    Renamed service files to valid Python module names

---
+++ services/_init_.py
@@ -0,0 +1,7 @@
+from .authentication_service import *
+from .admin_service import *
+from .candidate_service import *
+from .poll_service import *
+from .voter_service import *
+from .voting_service import *
+from .result_service import *
